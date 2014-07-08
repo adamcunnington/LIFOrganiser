d6cedecc75ceda60df7677e4ffcda82708133ab1
Adam Cunnington <adamcunnington.info@gmail.com>
2014-07-08 21:30:53 +0100

    Updates

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@
 
 from setuptools import setup
 
+
+def get_readme():
+    with open("README.md") as f:
+        return f.read()
+
+
 setup(name="LIFOrganiser",
       version="1.0",
       description="LearnItFirst.com Course Contents Organiser",
+      long_description=get_readme(),
       author="Adam Cunnington",
       author_email="ac@adamcunnington.info",
       license="GPLv3",
@@ -24,4 +31,6 @@ setup(name="LIFOrganiser",
           "Topic :: Education"],
       keywords="LearnItFirst Course Organiser File-Renamer",
       packages="liforganiser",
-      install_requires=["BeautifulSoup4", "requests"])
+      install_requires=[
+          "BeautifulSoup4",
+          "requests"])
